aeda6d50cfeb37dccdacdb33070c96cb39220f4c
Florian Matter <florianmatter@gmail.com>
2022-04-25 10:15:10 +0200

    cldf slice jsons

---
+++ src/pylingdocs/cldf.py
@@ -0,0 +1,14 @@
+from clldutils import jsonlib
+from pylingdocs.config import DATA_DIR
+from pathlib import Path
+import pycldf
+
+def metadata(table_name):
+    path = DATA_DIR / "cldf" / f"{table_name}-metadata.json"
+    if not path.is_file():
+        path = (
+            Path(pycldf.__file__).resolve().parent
+            / "components"
+            / f"{table_name}-metadata.json"
+            )
+    return jsonlib.load(path)
