09004e3a80e54900e80afdd2b628e86ef5795db2
Florian Matter <florianmatter@gmail.com>
2022-04-27 10:04:30 +0200

    testing split_ref

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,4 @@
-from pylingdocs.helpers import _get_relative_file
+from pylingdocs.helpers import _get_relative_file, split_ref
 from pathlib import Path
 
 
@@ -9,3 +9,7 @@ def test_structure():
     assert _get_relative_file(
         folder="my_own_contents", file="path/my_own_structure.yaml"
     ) == Path("path/my_own_structure.yaml")
+
+def test_refsplit():
+    assert split_ref("anon1998grammar[301]") == ("anon1998grammar", "301")
+    assert split_ref("anon1998grammar") == ("anon1998grammar", None)
