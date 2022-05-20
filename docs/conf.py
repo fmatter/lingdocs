0bfe27daa818797cf666616e0b8f493542838611
Florian Matter <florianmatter@gmail.com>
2022-05-20 09:31:41 -0700

    bump

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Florian Matter"
 author = "Florian Matter"
 
 # The full version, including alpha/beta/rc tags
-version = release = "0.0.2"
+version = release = "0.0.3.dev"
 
 # -- General configuration ---------------------------------------------------
 
