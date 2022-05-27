4e172305b1b5d3ebdef7318424a3ab26c083f70c
Florian Matter <florianmatter@gmail.com>
2022-05-27 09:09:17 -0700

    release 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Florian Matter"
 author = "Florian Matter"
 
 # The full version, including alpha/beta/rc tags
-version = release = "0.0.3.dev"
+version = release = "0.0.3"
 
 # -- General configuration ---------------------------------------------------
 
